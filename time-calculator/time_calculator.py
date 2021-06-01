24ed22ab6e8393b10792812902e1b55cc118dc72
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-06-01 12:27:44 -0600

    update time calc to pass first test

--- time-calculator/time_calculator.py
+++ time-calculator/time_calculator.py
@@ -1,7 +1,23 @@
+def convertToTime(hour, min):
+  period = 'AM'
+  if hour > 12:
+    hour = hour%12
+    period = 'PM'
+  return f'{hour}:{min} {period}'
+
 def add_time(start, duration):
+  start = start.split(" ")
+  
+  # split and convert array items to ints
+  hours, mins = map(int,start[0].split(':'))
+  dHours, dMins = map(int,duration.split(':'))
 
+  if (start[1].lower() == 'pm'):
+    # Time is afternoon
+    hours = hours + 12
 
-
-
-
-    return new_time
+  hours = hours + dHours
+  mins = mins + dMins
+  
+  new_time = convertToTime(hours, mins)
+  return new_time
