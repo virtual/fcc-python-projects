ac4532ccbe6c136ca3b644008079aec191bdfaa5
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-06-02 18:38:55 -0600

    python project #2 tests 2-5 working

--- time-calculator/test_module.py
+++ time-calculator/test_module.py
@@ -9,25 +9,25 @@ class UnitTests(unittest.TestCase):
         expected = "5:42 PM"
         self.assertEqual(actual, expected, 'Expected calling "add_time()" with "3:30 PM", "2:12" to return "5:42 PM"')
 
-    # def test_different_period(self):
-    #     actual = add_time("11:55 AM", "3:12")
-    #     expected = "3:07 PM"
-    #     self.assertEqual(actual, expected, 'Expected calling "add_time()" with "11:55 AM", "3:12" to return "3:07 PM"')
+    def test_different_period(self):
+        actual = add_time("11:55 AM", "3:12")
+        expected = "3:07 PM"
+        self.assertEqual(actual, expected, 'Expected calling "add_time()" with "11:55 AM", "3:12" to return "3:07 PM"')
 
-    # def test_next_day(self):
-    #     actual = add_time("9:15 PM", "5:30")
-    #     expected = "2:45 AM (next day)"
-    #     self.assertEqual(actual, expected, 'Expected time to end with "(next day)" when it is the next day.')
+    def test_next_day(self):
+        actual = add_time("9:15 PM", "5:30")
+        expected = "2:45 AM (next day)"
+        self.assertEqual(actual, expected, 'Expected time to end with "(next day)" when it is the next day.')
 
-    # def test_period_change_at_twelve(self):
-    #     actual = add_time("11:40 AM", "0:25")
-    #     expected = "12:05 PM"
-    #     self.assertEqual(actual, expected, 'Expected period to change from AM to PM at 12:00')
+    def test_period_change_at_twelve(self):
+        actual = add_time("11:40 AM", "0:25")
+        expected = "12:05 PM"
+        self.assertEqual(actual, expected, 'Expected period to change from AM to PM at 12:00')
 
-    # def test_twenty_four(self):
-    #     actual = add_time("2:59 AM", "24:00")
-    #     expected = "2:59 AM (next day)"
-    #     self.assertEqual(actual, expected, 'Expected calling "add_time()" with "2:59 AM", "24:00" to return "2:59 AM"')
+    def test_twenty_four(self):
+        actual = add_time("2:59 AM", "24:00")
+        expected = "2:59 AM (next day)"
+        self.assertEqual(actual, expected, 'Expected calling "add_time()" with "2:59 AM", "24:00" to return "2:59 AM"')
 
     # def test_two_days_later(self):
     #     actual = add_time("11:59 PM", "24:05")
--- time-calculator/time_calculator.py
+++ time-calculator/time_calculator.py
@@ -1,9 +1,21 @@
-def convertToTime(hour, min):
+import math
+
+def convertToTime(hour, min, days):
   period = 'AM'
-  if hour > 12:
-    hour = hour%12
+  dayStr = ''
+  hour12 = hour
+  if hour >= 12:
+    hour12 = hour%12
     period = 'PM'
-  return f'{hour}:{min} {period}'
+  if hour12 == 0:
+    hour12 = 12
+  if days == 1:
+    dayStr = ' (next day)'
+  elif days > 1:
+    dayStr = ' (' + days + ' days later)'
+  
+  return f'{hour12}:{str(min).zfill(2)} {period}{dayStr}'
+  # return f'{hour}:{str(min).zfill(2)} {period} ({days} Days)'
 
 def add_time(start, duration):
   start = start.split(" ")
@@ -16,8 +28,12 @@ def add_time(start, duration):
     # Time is afternoon
     hours = hours + 12
 
-  hours = hours + dHours
-  mins = mins + dMins
+  # Need to check if the clock reaches midnight instead of duration being 24 hours
+  daysOf = math.floor((hours + dHours) / 24)
+  # return f'h: {(hours + dHours)%24}, {dHours} | {math.floor((mins + dMins)/60)} |  m: {mins}, {dMins} - ({days} Days)'
+  minsOf = (mins + dMins)%60 #R
+  hoursOf = (hours + dHours)%24 + math.floor((mins + dMins)/60) #Could be more than 24
   
-  new_time = convertToTime(hours, mins)
+  
+  new_time = convertToTime(hoursOf, minsOf, daysOf)
   return new_time
