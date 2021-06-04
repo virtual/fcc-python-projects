d685b34edd825ab7f620453b387bda47443b0bca
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-06-03 18:40:40 -0600

    python proj #2 passing all test

--- time-calculator/test_module.py
+++ time-calculator/test_module.py
@@ -29,40 +29,40 @@ class UnitTests(unittest.TestCase):
         expected = "2:59 AM (next day)"
         self.assertEqual(actual, expected, 'Expected calling "add_time()" with "2:59 AM", "24:00" to return "2:59 AM"')
 
-    # def test_two_days_later(self):
-    #     actual = add_time("11:59 PM", "24:05")
-    #     expected = "12:04 AM (2 days later)"
-    #     self.assertEqual(actual, expected, 'Expected calling "add_time()" with "11:59 PM", "24:05" to return "12:04 AM (2 days later)"')
+    def test_two_days_later(self):
+        actual = add_time("11:59 PM", "24:05")
+        expected = "12:04 AM (2 days later)"
+        self.assertEqual(actual, expected, 'Expected calling "add_time()" with "11:59 PM", "24:05" to return "12:04 AM (2 days later)"')
 
-    # def test_high_duration(self):
-    #     actual = add_time("8:16 PM", "466:02")
-    #     expected = "6:18 AM (20 days later)"
-    #     self.assertEqual(actual, expected, 'Expected calling "add_time()" with "8:16 PM", "466:02" to return "6:18 AM (20 days later)"')
+    def test_high_duration(self):
+        actual = add_time("8:16 PM", "466:02")
+        expected = "6:18 AM (20 days later)"
+        self.assertEqual(actual, expected, 'Expected calling "add_time()" with "8:16 PM", "466:02" to return "6:18 AM (20 days later)"')
 
-    # def test_no_change(self):
-    #     actual = add_time("5:01 AM", "0:00")
-    #     expected = "5:01 AM"
-    #     self.assertEqual(actual, expected, 'Expected adding 0:00 to return initial time.')
+    def test_no_change(self):
+        actual = add_time("5:01 AM", "0:00")
+        expected = "5:01 AM"
+        self.assertEqual(actual, expected, 'Expected adding 0:00 to return initial time.')
 
-    # def test_same_period_with_day(self):
-    #     actual = add_time("3:30 PM", "2:12", "Monday")
-    #     expected = "5:42 PM, Monday"
-    #     self.assertEqual(actual, expected, 'Expected calling "add_time()" with "3:30 PM", "2:12", "Monday" to return "5:42 PM, Monday"')
+    def test_same_period_with_day(self):
+        actual = add_time("3:30 PM", "2:12", "Monday")
+        expected = "5:42 PM, Monday"
+        self.assertEqual(actual, expected, 'Expected calling "add_time()" with "3:30 PM", "2:12", "Monday" to return "5:42 PM, Monday"')
 
-    # def test_twenty_four_with_day(self):
-    #     actual = add_time("2:59 AM", "24:00", "saturDay")
-    #     expected = "2:59 AM, Sunday (next day)"
-    #     self.assertEqual(actual, expected, 'Expected calling "add_time()" with "2:59 AM", "24:00", "saturDay" to return "2:59 AM, Sunday (next day)"')
+    def test_twenty_four_with_day(self):
+        actual = add_time("2:59 AM", "24:00", "saturDay")
+        expected = "2:59 AM, Sunday (next day)"
+        self.assertEqual(actual, expected, 'Expected calling "add_time()" with "2:59 AM", "24:00", "saturDay" to return "2:59 AM, Sunday (next day)"')
 
-    # def test_two_days_later_with_day(self):
-    #     actual = add_time("11:59 PM", "24:05", "Wednesday")
-    #     expected = "12:04 AM, Friday (2 days later)"
-    #     self.assertEqual(actual, expected, 'Expected calling "add_time()" with "11:59 PM", "24:05", "Wednesday" to return "12:04 AM, Friday (2 days later)"')
+    def test_two_days_later_with_day(self):
+        actual = add_time("11:59 PM", "24:05", "Wednesday")
+        expected = "12:04 AM, Friday (2 days later)"
+        self.assertEqual(actual, expected, 'Expected calling "add_time()" with "11:59 PM", "24:05", "Wednesday" to return "12:04 AM, Friday (2 days later)"')
 
-    # def test_high_duration_with_day(self):
-    #     actual = add_time("8:16 PM", "466:02", "tuesday")
-    #     expected = "6:18 AM, Monday (20 days later)"
-    #     self.assertEqual(actual, expected, 'Expected calling "add_time()" with "8:16 PM", "466:02", "tuesday" to return "6:18 AM, Monday (20 days later)"')
+    def test_high_duration_with_day(self):
+        actual = add_time("8:16 PM", "466:02", "tuesday")
+        expected = "6:18 AM, Monday (20 days later)"
+        self.assertEqual(actual, expected, 'Expected calling "add_time()" with "8:16 PM", "466:02", "tuesday" to return "6:18 AM, Monday (20 days later)"')
 
 if __name__ == "__main__":
     unittest.main()
--- time-calculator/time_calculator.py
+++ time-calculator/time_calculator.py
@@ -1,8 +1,51 @@
 import math
 
-def convertToTime(hour, min, days):
+# given a certain day (0 = Monday) and duration (1 day)
+# add the duration to the day #, and mod by 6 to return
+# the updated day name
+
+# Return the day name of an equivalent day index #
+def getDayNameByNum(dayIndex):
+  if (dayIndex == 0): 
+    return 'Monday'
+  elif (dayIndex == 1):
+    return 'Tuesday'
+  elif (dayIndex == 2):
+    return 'Wednesday'
+  elif (dayIndex == 3):
+    return 'Thursday'
+  elif (dayIndex == 4):
+    return 'Friday'
+  elif (dayIndex == 5):
+    return 'Saturday'
+  elif (dayIndex == 6):
+    return 'Sunday'
+
+# given a day of the week, return the day's index #
+def getDayNum(day):
+  day = (day).lower()
+  if (day == 'monday'):
+    return 0
+  elif (day == 'tuesday'):
+    return 1
+  elif (day == 'wednesday'):
+    return 2
+  elif (day == 'thursday'):
+    return 3
+  elif (day == 'friday'):
+    return 4
+  elif (day == 'saturday'):
+    return 5
+  elif (day == 'sunday'):
+    return 6
+
+def convertToTime(hour, min, days=0, startDay=''):
   period = 'AM'
   dayStr = ''
+  endDay = ''
+  endDayNum = -1
+  if (startDay != ''):
+    endDayNum = getDayNum(startDay)
   hour12 = hour
   if hour >= 12:
     hour12 = hour%12
@@ -12,12 +55,16 @@ def convertToTime(hour, min, days):
   if days == 1:
     dayStr = ' (next day)'
   elif days > 1:
-    dayStr = ' (' + days + ' days later)'
+    dayStr = ' (' + str(days) + ' days later)'
   
-  return f'{hour12}:{str(min).zfill(2)} {period}{dayStr}'
+  if endDayNum > -1:
+    endDay = (endDayNum + days) % 7
+    endDay = ', ' + getDayNameByNum(endDay)
+  
+  return f'{hour12}:{str(min).zfill(2)} {period}{endDay}{dayStr}'
   # return f'{hour}:{str(min).zfill(2)} {period} ({days} Days)'
 
-def add_time(start, duration):
+def add_time(start, duration, startDay=''):
   start = start.split(" ")
   
   # split and convert array items to ints
@@ -29,11 +76,17 @@ def add_time(start, duration):
     hours = hours + 12
 
   # Need to check if the clock reaches midnight instead of duration being 24 hours
+  # Well this is saying duration of hours (like 8 hour day) + startHours, so if
+  # this over 24, it is the next day
   daysOf = math.floor((hours + dHours) / 24)
+  
   # return f'h: {(hours + dHours)%24}, {dHours} | {math.floor((mins + dMins)/60)} |  m: {mins}, {dMins} - ({days} Days)'
   minsOf = (mins + dMins)%60 #R
   hoursOf = (hours + dHours)%24 + math.floor((mins + dMins)/60) #Could be more than 24
   
+  if hoursOf >= 24:
+    daysOf = daysOf + math.floor(hoursOf/24)
+    hoursOf = hoursOf%24
   
-  new_time = convertToTime(hoursOf, minsOf, daysOf)
+  new_time = convertToTime(hoursOf, minsOf, daysOf, startDay)
   return new_time
