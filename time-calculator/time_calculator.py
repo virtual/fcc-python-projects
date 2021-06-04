54691248d3b635dadb0ea9582deb39746509b9f0
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-06-03 18:45:10 -0600

    cleanup

--- time-calculator/time_calculator.py
+++ time-calculator/time_calculator.py
@@ -1,9 +1,5 @@
 import math
 
-# given a certain day (0 = Monday) and duration (1 day)
-# add the duration to the day #, and mod by 6 to return
-# the updated day name
-
 # Return the day name of an equivalent day index #
 def getDayNameByNum(dayIndex):
   if (dayIndex == 0): 
@@ -39,6 +35,9 @@ def getDayNum(day):
   elif (day == 'sunday'):
     return 6
 
+# given a certain day (0 = Monday) and duration (1 day)
+# add the duration to the day #, and mod by 6 to return
+# the updated day name
 def convertToTime(hour, min, days=0, startDay=''):
   period = 'AM'
   dayStr = ''
@@ -48,7 +47,7 @@ def convertToTime(hour, min, days=0, startDay=''):
     endDayNum = getDayNum(startDay)
   hour12 = hour
   if hour >= 12:
-    hour12 = hour%12
+    hour12 = hour % 12
     period = 'PM'
   if hour12 == 0:
     hour12 = 12
@@ -62,7 +61,6 @@ def convertToTime(hour, min, days=0, startDay=''):
     endDay = ', ' + getDayNameByNum(endDay)
   
   return f'{hour12}:{str(min).zfill(2)} {period}{endDay}{dayStr}'
-  # return f'{hour}:{str(min).zfill(2)} {period} ({days} Days)'
 
 def add_time(start, duration, startDay=''):
   start = start.split(" ")
@@ -75,18 +73,15 @@ def add_time(start, duration, startDay=''):
     # Time is afternoon
     hours = hours + 12
 
-  # Need to check if the clock reaches midnight instead of duration being 24 hours
-  # Well this is saying duration of hours (like 8 hour day) + startHours, so if
-  # this over 24, it is the next day
+  # Check if clock reaches midnight
   daysOf = math.floor((hours + dHours) / 24)
   
-  # return f'h: {(hours + dHours)%24}, {dHours} | {math.floor((mins + dMins)/60)} |  m: {mins}, {dMins} - ({days} Days)'
-  minsOf = (mins + dMins)%60 #R
-  hoursOf = (hours + dHours)%24 + math.floor((mins + dMins)/60) #Could be more than 24
+  minsOf = (mins + dMins) % 60 
+  hoursOf = (hours + dHours) % 24 + math.floor((mins + dMins) / 60) # Could be more than 24
   
   if hoursOf >= 24:
-    daysOf = daysOf + math.floor(hoursOf/24)
-    hoursOf = hoursOf%24
+    daysOf = daysOf + math.floor(hoursOf / 24)
+    hoursOf = hoursOf % 24
   
   new_time = convertToTime(hoursOf, minsOf, daysOf, startDay)
   return new_time
