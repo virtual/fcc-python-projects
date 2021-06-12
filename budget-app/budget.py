89f095a93f2289ed4b344f320e355f9551e59738
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-06-11 23:16:26 -0600

    add some python args

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -1,7 +1,18 @@
 class Category:
-
-
-
-
+    def __init__(self, Food = "", Entertainment = "", Business = ""):
+        self.food = Food
+        self.entertainment = Entertainment
+        self.business = Business
+        
+        self.deposit = deposit
+        self.withdraw = withdraw
+        self.get_balance = get_balance
+        self.transfer = transfer
+        self.check_funds = check_funds
 
 def create_spend_chart(categories):
+    self.deposit = deposit
+    self.withdraw = withdraw
+    self.get_balance = get_balance
+    self.transfer = transfer
+    self.check_funds = check_funds
