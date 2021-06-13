d0b9cdb7cb8aed5dfeba44786b0ef1f7e94a009b
Jeanine Schoessler <jeanine.mt@gmail.com>
2021-06-13 09:26:16 -0600

    proj 3 tests for printing instance, etc

--- budget-app/budget.py
+++ budget-app/budget.py
@@ -1,10 +1,39 @@
 class Category:
-  # categories = ''
+  categories = []
   # ledger = []
+  # name = ''
   
-  def __init__(self, categories):
-    self.categories = categories
+  # When the budget object is printed it should display:
+  # * A title line of 30 characters where the name of the category is centered in a line of `*` characters.
+  # * A list of the items in the ledger. Each line should show the description and amount. The first 23 characters of the description should be displayed, then the amount. The amount should be right aligned, contain two decimal places, and display a maximum of 7 characters.
+  # * A line displaying the category total.
+  # Here is an example of the output:
+  # ```
+  # *************Food*************
+  # initial deposit        1000.00
+  # groceries               -10.15
+  # restaurant and more foo -15.89
+  # Transfer to Clothing    -50.00
+  # Total: 923.96
+  # ```
+  
+  def __str__(self):
+    output = ''
+    for iter in range(len(self.ledger)):
+      amount = float(self.ledger[iter]['amount'])
+      amtCurrency = str("{:.2f}".format(amount))
+      description = (self.ledger[iter]['description'])
+      descShort = (description[:23]) if len(description) > 23 else description.ljust(23)
+      output = output + "\n" + descShort + ' ' + amtCurrency
+    output = output + "\nTotal: " + str(self.get_balance())
+
+    ## !TODO: Need to center the title based on length
+    return f'*************{self.name}*************{output}'
+
+  def __init__(self, category):
+    self.categories.append(category)
     self.ledger = []
+    self.name = category
     
   def deposit(self, amount, note = ''):
     line = {"amount": amount, "description": note}
@@ -32,8 +61,20 @@ class Category:
     return total
 
   def transfer(self, amount, targetCategory):
-    # self.ledger.append('x')
-    print(self.ledger)
+    '''
+    Send a message to a recipient
+
+    :param float amount: The amount of money to transfer
+    :param str targetCategory: The target category to transfer to
+    :return: `True` if the transfer took place, and `False` otherwise
+    :rtype: boolean
+    '''
+    if (self.check_funds(amount)):
+      self.withdraw(amount, "Transfer to " + targetCategory.name)
+      targetCategory.deposit(amount, "Transfer from " + self.name)
+      return True
+    
+    return False
 
   # It returns `False` if the amount is greater than the balance of the budget category and returns `True` otherwise. This method should be used by both the `withdraw` method and `transfer` method.
   def check_funds(self, amount=0):
@@ -41,11 +82,8 @@ class Category:
       return True
     else:
       return False
-    # self.ledger.append('x')
-    # print(self.ledger)
 
 def create_spend_chart(self, categories =[]):
-    # self.ledger.append('x')
     print('100  Mew mew')
       
   #   self.deposit = deposit
--- budget-app/test_module.py
+++ budget-app/test_module.py
@@ -43,17 +43,17 @@ class UnitTests(unittest.TestCase):
         expected = 854.33
         self.assertEqual(actual, expected, 'Expected balance to be 854.33')
 
-    # def test_transfer(self):
-    #     self.food.deposit(900, "deposit")
-    #     self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-    #     good_transfer = self.food.transfer(20, self.entertainment)
-    #     actual = self.food.ledger[2]
-    #     expected = {"amount": -20, "description": "Transfer to Entertainment"}
-    #     self.assertEqual(actual, expected, 'Expected `transfer` method to create a specific ledger item in food object.')
-    #     self.assertEqual(good_transfer, True, 'Expected `transfer` method to return `True`.')
-    #     actual = self.entertainment.ledger[0]
-    #     expected = {"amount": 20, "description": "Transfer from Food"}
-    #     self.assertEqual(actual, expected, 'Expected `transfer` method to create a specific ledger item in entertainment object.')
+    def test_transfer(self):
+        self.food.deposit(900, "deposit")
+        self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+        good_transfer = self.food.transfer(20, self.entertainment)
+        actual = self.food.ledger[2]
+        expected = {"amount": -20, "description": "Transfer to Entertainment"}
+        self.assertEqual(actual, expected, 'Expected `transfer` method to create a specific ledger item in food object.')
+        self.assertEqual(good_transfer, True, 'Expected `transfer` method to return `True`.')
+        actual = self.entertainment.ledger[0]
+        expected = {"amount": 20, "description": "Transfer from Food"}
+        self.assertEqual(actual, expected, 'Expected `transfer` method to create a specific ledger item in entertainment object.')
 
     def test_check_funds(self):
         self.food.deposit(10, "deposit")
@@ -69,18 +69,18 @@ class UnitTests(unittest.TestCase):
         good_withdraw = self.food.withdraw(100.10)
         self.assertEqual(good_withdraw, False, 'Expected `withdraw` method to return `False`.')
 
-    # def test_transfer_no_funds(self):
-    #     self.food.deposit(100, "deposit")
-    #     good_transfer = self.food.transfer(200, self.entertainment)
-    #     self.assertEqual(good_transfer, False, 'Expected `transfer` method to return `False`.')
+    def test_transfer_no_funds(self):
+        self.food.deposit(100, "deposit")
+        good_transfer = self.food.transfer(200, self.entertainment)
+        self.assertEqual(good_transfer, False, 'Expected `transfer` method to return `False`.')
 
-    # def test_to_string(self):
-    #     self.food.deposit(900, "deposit")
-    #     self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
-    #     self.food.transfer(20, self.entertainment)
-    #     actual = str(self.food)
-    #     expected = f"*************Food*************\ndeposit                 900.00\nmilk, cereal, eggs, bac -45.67\nTransfer to Entertainme -20.00\nTotal: 834.33"
-    #     self.assertEqual(actual, expected, 'Expected different string representation of object.')
+    def test_to_string(self):
+        self.food.deposit(900, "deposit")
+        self.food.withdraw(45.67, "milk, cereal, eggs, bacon, bread")
+        self.food.transfer(20, self.entertainment)
+        actual = str(self.food)
+        expected = f"*************Food*************\ndeposit                 900.00\nmilk, cereal, eggs, bac -45.67\nTransfer to Entertainme -20.00\nTotal: 834.33"
+        self.assertEqual(actual, expected, 'Expected different string representation of object.')
 
     # def test_create_spend_chart(self):
     #     self.food.deposit(900, "deposit")
